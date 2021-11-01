9b763cd5ff31c6fa06f62914687a2840822ccae4
glauberdmo <glauberdmo@gmail.com>
2021-11-01 01:43:01 -0300

    added function to create menu -- progress

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 """
 Platformer Game
 """
+
 import arcade
+import arcade.gui
+from Interface.OptionsDialog import OptionsDialog
 
 # Constants
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
+SCREEN_WIDTH = 1400
+SCREEN_HEIGHT = 1000
 SCREEN_TITLE = "Platformer"
-FULL_SCREEN = True
+FULL_SCREEN = False
 
 class MyGame(arcade.Window):
     """
@@ -18,18 +21,36 @@ class MyGame(arcade.Window):
 
         # Call the parent class and set up the window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE,FULL_SCREEN)
-
         arcade.set_background_color((20, 20, 20))
+
+        self.textOptions =[]
+        self.NavigationBox = None
+        self.shape_list = None
+        self.textBox = None
+        
+        
 
     def setup(self):
         """Set up the game here. Call this function to restart the game."""
-        pass        
+        self.textOptions.append("Start Game")
+        self.textOptions.append("Options")
+        self.textOptions.append("Exit")
 
+        self.NavigationBox = OptionsDialog(self.textOptions, 300,500,600,100,10)
+
+        print(self.NavigationBox.get_colors())
+        self.shape = arcade.create_rectangles_filled_with_colors(self.NavigationBox.get_points(),self.NavigationBox.get_colors())
+        
+        self.shape_list = arcade.ShapeElementList()
+        self.shape_list.append(self.shape)
+
+        self.textBox = arcade.gui.UIInputText(0,0,300,100,text_color=arcade.color.WHITE)
     def on_draw(self):
         """Render the screen."""
 
         arcade.start_render()
         # Code to draw the screen goes here
+        self.shape_list.draw()
 
 
 def main():
