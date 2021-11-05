835a3b23a3605dc0070615962e636c754cb6c0c5
glauberdmo <glauberdmo@gmail.com>
2021-11-05 20:06:47 -0300

    union unit w warrior <error found>

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     currentBoardgame = Boardgame(10,10)   
     warrior1 = Warrior(currentBoardgame,1,1)   
     warrior2 = Warrior(currentBoardgame,2,2)
+    warrior3 = Warrior(currentBoardgame,5,5)
     warrior1.move(2,2)
     currentBoardgame.get_unit_by_xy(1,1).attack(warrior2)
     warrior1.attack(currentBoardgame.get_unit_by_xy(2,2))
